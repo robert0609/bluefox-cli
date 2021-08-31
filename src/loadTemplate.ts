a7425f8138a4ab1897cdfa36c409acdad4a90848
robert <robert0609@163.com>
2021-08-31 16:21:00 +0800

    feat: 修改模板下载来源

--- src/loadTemplate.ts
+++ src/loadTemplate.ts
@@ -1,26 +1,23 @@
-import download from 'download-git-repo';
+import download from 'download';
 import path from 'path';
 import os from 'os';
 import ora from 'ora';
 import { isDirExist, removeDir } from './utility';
 
+const projectUrl = 'https://butterfly001.oss-cn-beijing.aliyuncs.com/project/fe-project.zip';
+
 export async function loadRemoteTemplate(repo: string): Promise<string> {
   const spinner = ora('fetching project template...');
   spinner.start();
   try {
-    return await new Promise((resolve, reject) => {
-      const tmpDir = path.normalize(path.join(os.tmpdir(), 'bluefox-template'));
-      if (isDirExist(tmpDir)) {
-        removeDir(tmpDir);
-      }
-      download(repo, tmpDir, err => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(tmpDir);
-        }
-      });
+    const tmpDir = path.normalize(path.join(os.tmpdir(), 'bluefox-template'));
+    if (isDirExist(tmpDir)) {
+      removeDir(tmpDir);
+    }
+    await download(projectUrl, tmpDir, {
+      extract: true
     });
+    return tmpDir;
   } finally {
     spinner.stop();
   }
